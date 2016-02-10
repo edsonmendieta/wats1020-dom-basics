9415646264c35f20001eff9b617f59f7e3dcd78f
emendietaj <emendietaj@gmail.com>
2016-02-10 00:15:04 -0500

    deleted conflict text in .js file

--- fortune-cookie-generator.js
+++ fortune-cookie-generator.js
@@ -10,10 +10,7 @@ function generateFortuneCookie() { //function that executes every time user clic
   	 }
   	 if (usedFortunes.length === 0) {
     	 usedFortunes.push(getFortune);
-<<<<<<< HEAD
-=======
 			 return getFortune;
->>>>>>> master
   	 }   
   	 else {
     	 for (i = 0; i < usedFortunes.length; ++i) {
@@ -23,10 +20,7 @@ function generateFortuneCookie() { //function that executes every time user clic
       	 }  
     	 }
     	 usedFortunes.push(getFortune);
-<<<<<<< HEAD
-=======
 			 return getFortune;
->>>>>>> master
   	 }
 	 }
 
